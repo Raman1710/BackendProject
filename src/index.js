61b3347bbda3bb7af86f567e8205a38976a69bf7
Ramandeep Singh Saini <sainiramandeep1710@gmail.com>
2024-12-20 20:39:37 +0530

    connected to db

---
+++ src/index.js
@@ -0,0 +1,11 @@
+//  import dotenv from "dotenv"
+import 'dotenv/config'
+
+import express from "express"
+import connectDB from "./db/db.js"
+
+// dotenv.config({
+//     path: './.env'
+// })
+
+connectDB(); 
