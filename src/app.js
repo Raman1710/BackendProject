78606dbfb743c49745fcd9dd245fe69751469bcb
Ramandeep Singh Saini <sainiramandeep1710@gmail.com>
2024-12-22 19:26:29 +0530

    added register controller and updated other files

--- src/app.js
+++ src/app.js
@@ -11,16 +11,23 @@ app.use(cors({
 }));
 
 app.use(express.json({
-    limit:"16kb"
+    limit: "16kb"
 }));
 
 app.use(urlencoded({
-    extended:true,
-    limit:"16kb"
+    extended: true,
+    limit: "16kb"
 }));
 
 app.use(express.static("public"));
 
-app.use(cookieParser);
+app.use(cookieParser());
 
-export {app}
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+export { app }
